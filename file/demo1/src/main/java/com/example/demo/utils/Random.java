85fd0cf12ce0d39b6deeaa79bb2b033eb56c49fc
Wangbangc <89343099+Wangbangc@users.noreply.github.com>
2024-04-28 12:55:24 +0800

    Delete file directory

--- file/demo1/src/main/java/com/example/demo/utils/Random.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.utils;
-
-public class Random {
-    public static void main(String[] args) {
-        java.util.Random rand = new java.util.Random();
-        int num = rand.nextInt(900000000) + 100000000;
-        System.out.println(num);
-    }
-}
